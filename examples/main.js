9d4a5d4781144cbeb8405d378b8d01395fc69579
jurgob <jurgo.boemo@gmail.com>
2015-01-04 15:49:14 +0000

    update example with ng-repeat case

--- examples/main.js
+++ examples/main.js
@@ -4,4 +4,8 @@ testBingMap.controller('MainCtrl', function ($scope) {
 
     $scope.title = "test test";
     $scope.center = [43.000, 13.0000]
+    $scope.markers = [[43.000, 13.0000], [43.0001, 13.0001], [43.0002, 13.0002]]
+    $scope.markers2 = [[43.0002, 13.0000]]
+
+
 });
